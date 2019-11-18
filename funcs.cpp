3a6e65b12795448b19e3ffe4db42428a80bfc694
allenbars <allenbars@yandex.ru>
2019-11-18 21:01:10 +0300

    add function MyPow() version 2

--- funcs.cpp
+++ funcs.cpp
@@ -1,4 +1,7 @@
-//funcs.cpp #include "stdafx.h" #include "funcs.h"  
+//funcs.cpp 
+#include "stdafx.h" 
+#include "funcs.h" 
+#include <cmath>  
  
 double MySum(double a, double b) 
 {  return a + b; } 
@@ -9,3 +12,5 @@ double MyMul(double a, double b)
 {  return a * b; } 
 double MyDiv(double a, double b) 
 {  return a / b; } 
+double MyPow(double a, double b) 
+{  return exp(b * log(a)); }
